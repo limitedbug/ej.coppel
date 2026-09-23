b4f63672c9c14efcde5b6bf17559b1fb3f0e9b90
Roberto <mrbugexe@gmail.com>
2026-09-22 18:40:37 -0600

    feat: add show comments and ratings and fix validate show before save comment

---
+++ src/main/java/com/coppel/tvmaze_middleware/model/mongo/Comment.java
@@ -0,0 +1,4 @@
+package com.coppel.tvmaze_middleware.model.mongo;
+
+public class Comment {
+}
---
+++ src/main/java/com/coppel/tvmaze_middleware/repository/ShowCommentsRepository.java
@@ -0,0 +1,4 @@
+package com.coppel.tvmaze_middleware.repository;
+
+public class ShowCommentsRepository {
+}
